bde41fcf9730e048156e5ce10a6656a7df8d31bc
woodwardmw <woodwardmw@gmail.com>
2021-11-01 20:55:51 +0000

    Added Procfile and changed port information in predict_vowel.py

--- predict_vowel.py
+++ predict_vowel.py
@@ -42,5 +42,5 @@ def convert_to_vowel(vowel_int):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port) 
