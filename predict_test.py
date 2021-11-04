14a5d08b7f8c2a06951b62ca6b3f9cac677b19c7
woodwardmw <woodwardmw@gmail.com>
2021-11-04 21:44:42 +0000

    predict_test.py now working

--- predict_test.py
+++ predict_test.py
@@ -28,13 +28,10 @@ df_test = df[df.speaker.isin([10, 11, 13, 14])]  # Two male and two female in df
 random_index = random.randint(0, df_test.shape[0] - 1)
 
 example = df_test.iloc[[random_index,]]
-print(example)
-print(type(example))
 vowel_int = example.iloc[0]['vowel'].astype(int)
 print(f'Actual vowel: {convert_to_vowel(vowel_int)}')
 
-example_dict = example.to_dict()
-print(example_dict)
+example_dict = example.drop(['speaker', 'vowel'], axis=1).to_dict()
 url = 'https://zoomcamp-midterm-heroku.herokuapp.com/predict'
 response = requests.post(url, json=example_dict)
 result = response.json()
